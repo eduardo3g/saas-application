12a1c140a8f47f81a480e9f3b6adc5d208ac0596
= <=>
2020-03-14 16:04:27 -0300

    Added Slugify trait on Teams model in order to crate the team slug on creation time.

--- backend/app/Models/Team.js
+++ backend/app/Models/Team.js
@@ -1,6 +1,18 @@
 const Model = use('Model')
 
 class Team extends Model {
+  static boot () {
+    super.boot()
+
+    this.addTrait('@provider:Lucid/Slugify', {
+      fields: {
+        slug: 'name' // slug - column name on 'teams' table | name - slug will be created based on name column value
+      },
+      strategy: 'dbIncrement',
+      disableUpdates: false // only sets slug when the team is created
+    })
+  }
+
   users () {
     return this.belongsToMany('App/Models/User').pivotModel(
       'App/Models/UserTeam'
