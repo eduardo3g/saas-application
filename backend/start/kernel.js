b394c212750c67708d22e69f0e2fcf06e43afb3b
= <=>
2020-03-14 20:53:58 -0300

    feat: Teams middleware.

--- backend/start/kernel.js
+++ backend/start/kernel.js
@@ -35,7 +35,8 @@ const globalMiddleware = [
 */
 const namedMiddleware = {
   auth: 'Adonis/Middleware/Auth',
-  guest: 'Adonis/Middleware/AllowGuestOnly'
+  guest: 'Adonis/Middleware/AllowGuestOnly',
+  team: 'App/Middleware/Team'
 }
 
 /*
