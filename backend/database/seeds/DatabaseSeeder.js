6d15f0d47f6b7188cefc58b32f41076ea06e3c00
= <=>
2020-03-21 19:50:08 -0300

    feat: Updated DatabaseSeeder to implement roles and permissions.

--- backend/database/seeds/DatabaseSeeder.js
+++ backend/database/seeds/DatabaseSeeder.js
@@ -12,6 +12,9 @@
 
 const User = use('App/Models/User')
 
+const Role = use('Adonis/Acl/Role')
+const Permission = use('Adonis/Acl/Permission')
+
 class DatabaseSeeder {
   async run () {
     const user = await User.create({
@@ -20,10 +23,48 @@ class DatabaseSeeder {
       password: '123456'
     })
 
-    await user.teams().create({
+    // Permissions
+    const createInvite = await Permission.create({
+      slug: 'invites_create',
+      name: 'Invite members'
+    })
+
+    const createProject = await Permission.create({
+      slug: 'projects_create',
+      name: 'Create projects'
+    })
+
+    // Roles
+    const admin = await Role.create({
+      slug: 'administrator',
+      name: 'Administrator'
+    })
+
+    const moderator = await Role.create({
+      slug: 'moderator',
+      name: 'Moderator'
+    })
+
+    // Has no permissions
+    await Role.create({
+      slug: 'visitor',
+      name: 'Visitor'
+    })
+
+    // Administrators can create invites and projects
+    await admin.permissions().attach([createInvite.id, createProject.id])
+
+    // Moderators can only create projects
+    await moderator.permissions().attach([createProject.id])
+
+    const team = await user.teams().create({
       name: 'LinkApi',
       user_id: user.id
     })
+
+    const teamJoin = await user.teamJoins().where('team_id', team.id).first()
+
+    await teamJoin.roles().attach([admin.id])
   }
 }
 
